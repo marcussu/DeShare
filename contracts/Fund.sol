e6b8c9a90961f7a83461f17c161a3a463b0ee75a
marcussu <marcussu@marcussu.com>
2021-02-19 14:10:29 +0800

    Removed artifacts for now until final version. Major revamp of contract structue for future maintainability. Added support for Binance wrapped USDT (IBEP40). Added FundPlatform to manage all funds. Added platform fee receiver vs fund's own fund manager.

--- contracts/Fund.sol
+++ contracts/Fund.sol
@@ -2,31 +2,167 @@
 
 pragma solidity >=0.7.0 <0.8.0;
 
-import "./IBEP20.sol";
+import "./BEP20.sol";
 
 /**
- * @title Manage the properties of a fund
- * @dev Set & change Fund Initiator
+ * @title BEP20Mintable
+ * @dev Implementation of the BEP20Mintable. Extension of {BEP20} that adds a minting behaviour.
  */
-contract Fund is Context, IBEP20, Ownable {
+abstract contract BEP20Mintable is BEP20 {
+
+    // indicates if minting is finished
+    bool private _mintingFinished = false;
+
+    /**
+     * @dev Emitted during finish minting
+     */
+    event MintFinished();
+
+    /**
+     * @dev Tokens can be minted only before minting finished.
+     */
+    modifier canMint() {
+        require(!_mintingFinished, "BEP20Mintable: minting is finished");
+        _;
+    }
+
+    /**
+     * @return if minting is finished or not.
+     */
+    function mintingFinished() public view returns (bool) {
+        return _mintingFinished;
+    }
+
+    /**
+     * @dev Function to mint tokens.
+     *
+     * WARNING: it allows everyone to mint new tokens. Access controls MUST be defined in derived contracts.
+     *
+     * @param account The address that will receive the minted tokens
+     * @param amount The amount of tokens to mint
+     */
+    function mint(address account, uint256 amount) public canMint {
+        _mint(account, amount);
+    }
+
+    /**
+     * @dev Function to stop minting new tokens.
+     *
+     * WARNING: it allows everyone to finish minting. Access controls MUST be defined in derived contracts.
+     */
+    function finishMinting() public canMint {
+        _finishMinting();
+    }
+
+    /**
+     * @dev Function to stop minting new tokens.
+     */
+    function _finishMinting() internal virtual {
+        _mintingFinished = true;
+
+        emit MintFinished();
+    }
+}
+
+/**
+ * @dev Extension of {BEP20} that allows token holders to destroy both their own
+ * tokens and those that they have an allowance for, in a way that can be
+ * recognized off-chain (via event analysis).
+ */
+abstract contract BEP20Burnable is Context, BEP20 {
+    using SafeMath for uint256;
+
+    /**
+     * @dev Destroys `amount` tokens from the caller.
+     *
+     * See {BEP20-_burn}.
+     */
+    function burn(uint256 amount) public virtual {
+        _burn(_msgSender(), amount);
+    }
+
+    /**
+     * @dev Destroys `amount` tokens from `account`, deducting from the caller's
+     * allowance.
+     *
+     * See {BEP20-_burn} and {BEP20-allowance}.
+     *
+     * Requirements:
+     *
+     * - the caller must have allowance for ``accounts``'s tokens of at least
+     * `amount`.
+     */
+    function burnFrom(address account, uint256 amount) public virtual {
+        uint256 decreasedAllowance = allowance(account, _msgSender()).sub(amount, "BEP20: burn amount exceeds allowance");
+
+        _approve(account, _msgSender(), decreasedAllowance);
+        _burn(account, amount);
+    }
+}
+
+/**
+ * @title FundManager
+ * @dev Implementation of the FundManager
+ */
+contract FundManager is Ownable {
+
+    mapping (bytes32 => uint256) private _prices;
+
+    event Created(string serviceName, address indexed serviceAddress);
+
+    function pay(string memory serviceName) public payable {
+        require(msg.value == _prices[_toBytes32(serviceName)], "FundManager: incorrect price");
+
+        emit Created(serviceName, _msgSender());
+    }
+
+    function getPrice(string memory serviceName) public view returns (uint256) {
+        return _prices[_toBytes32(serviceName)];
+    }
+
+    function setPrice(string memory serviceName, uint256 amount) public onlyOwner {
+        _prices[_toBytes32(serviceName)] = amount;
+    }
+
+    function withdraw(uint256 amount) public onlyOwner {
+        payable(owner()).transfer(amount);
+    }
+
+    function _toBytes32(string memory serviceName) private pure returns (bytes32) {
+        return keccak256(abi.encode(serviceName));
+    }
+}
+
+/**
+ * @title ServicePayer
+ * @dev Implementation of the ServicePayer
+ */
+abstract contract ServicePayer {
+
+    constructor (address payable receiver, string memory serviceName) payable {
+        require(receiver != address(0), "Fee receiver cannot be zero address.");
+        
+        FundManager(receiver).pay{value: msg.value}(serviceName);
+    }
+}
+
+/**
+ * @title Fund
+ * @dev Implementation of a decentralized mutual fund
+ */
+contract Fund is BEP20Mintable, BEP20Burnable, ServicePayer {
 
     using SafeMath for uint256;
-    
-    mapping (address => uint256) private _balances;
-    
-    mapping (address => mapping (address => uint256)) private _allowances;
-    
-    uint256 private _totalSupply;
-    uint8 private _decimals; // How many decimals to show. Standardized to 18 for A-Share
-    string private _symbol; // An identifier, required for BEP20. Will prefix with "A-" for all A-Share funds
-    string private _name; // Fund name, required for BEP20
-    
+
     address private fund_initiator;
     enum FundType { NEW_FUND, MONEY_MARKET_FUND, FIXED_INCOME_FUND, EQUITY_FUND, BALANCED_FUND, INDEX_FUND, SPECIALTY_FUND, FUND_OF_FUNDS }
     FundType private fund_type;
     enum FundStatus { INACTIVE, EXPRESS_INTEREST, ACTIVE }
     FundStatus private fund_status;
-    string asset_base_currency;
+    address private asset_base_currency;
+    bool public can_invest = true;
+    address payable public fee_receiver;
+    address payable public fund_manager;
     
     // events for EVM logging
     event InitiatorSet(address indexed oldInitiator, address indexed newInitiator);
@@ -39,269 +175,75 @@ contract Fund is Context, IBEP20, Ownable {
         // This used to consume all gas in old EVM versions, but not anymore.
         // It is often a good idea to use 'require' to check if functions are called correctly.
         // As a second argument, you can also provide an explanation about what went wrong.
-        require(msg.sender == fund_initiator, "Caller is not fund initiator");
+        require(_msgSender() == fund_initiator, "Caller is not fund initiator");
         _;
     }
     
     /**
-     * @dev Set contract deployer as initiator and check if fund type is valid
+     * @dev Create a decentralized mutual fund
      */
     constructor(
-        uint256 _initialAmount,
         string memory _tokenName,
         string memory _tokenSymbol,
-        FundType _fundType
-    ) payable { 
-        _name = _tokenName;                        // Set the name for display purposes
-        _symbol = _tokenSymbol;                    // Set the symbol for display purposes; Frontend should append "A-"
-        _decimals = 18;                            // Amount of decimals for display purposes; Fixed at 18 for all A-Share funds
-        _totalSupply = _initialAmount * 1000000000000000000;             // Update total supply
-        _balances[msg.sender] = _initialAmount * 1000000000000000000;    // Give the initiator all initial tokens
+        FundType _fundType,
+        address _assetBaseCurrency,
+        address payable _feeReceiver,
+        address payable _fundManager,
+        address _fundInitiator
+    )
+        BEP20(_tokenName, _tokenSymbol)
+        ServicePayer(_feeReceiver, "CREATE_FUND")
+        payable 
+    { 
+        require(_fundType >= FundType.MONEY_MARKET_FUND && fund_type <= FundType.FUND_OF_FUNDS, "Fund type defined is not supported.");
         
-        emit Transfer(address(0), msg.sender, _totalSupply);
+        //_setupDecimals(_decimals);                            // Amount of decimals for display purposes; Set to the default 18 for all A-Share funds
         
+        asset_base_currency = _assetBaseCurrency;                       // Can only be set once during fund setup; Immutable
         fund_status = FundStatus.ACTIVE;
         
-        fund_initiator = msg.sender; // 'msg.sender' is sender of current call, contract deployer for a constructor
+        fund_initiator = _fundInitiator; // Track the initiator of the fund
         emit InitiatorSet(address(0), fund_initiator);
         
         fund_type = _fundType;
-        require(fund_type >= FundType.MONEY_MARKET_FUND && fund_type <= FundType.FUND_OF_FUNDS, "Fund type defined is not supported.");
         emit FundTypeSet(FundType.NEW_FUND, fund_type);
+        
+        fee_receiver = _feeReceiver;
+        fund_manager = _fundManager;
     }
     
     /**
-    * @dev Returns the bep token owner.
-    */
-    function getOwner() external view override returns (address) {
-        return owner();
-    }
-    
-    /**
-    * @dev Returns the token decimals.
-    */
-    function decimals() external view override returns (uint8) {
-        return _decimals;
-    }
-    
-    /**
-    * @dev Returns the token symbol.
-    */
-    function symbol() external view override returns (string memory) {
-        return _symbol;
+     * @dev Function to mint tokens.
+     *
+     * NOTE: restricting access to owner only. See {BEP20Mintable-mint}.
+     *
+     * @param account The address that will receive the minted tokens
+     * @param amount The amount of tokens to mint
+     */
+    function _mint(address account, uint256 amount) internal override onlyOwner {
+        super._mint(account, amount);
     }
 
     /**
-    * @dev Returns the token name.
-    */
-    function name() external view override returns (string memory) {
-        return _name;
+     * @dev Function to stop minting new tokens.
+     *
+     * NOTE: restricting access to owner only. See {BEP20Mintable-finishMinting}.
+     */
+    function _finishMinting() internal override onlyOwner {
+        super._finishMinting();
     }
 
     /**
-    * @dev See {BEP20-totalSupply}.
-    */
-    function totalSupply() external view override returns (uint256) {
-        return _totalSupply;
-    }
-
-    /**
-    * @dev See {BEP20-balanceOf}.
-    */
-    function balanceOf(address account) external view override returns (uint256) {
-        return _balances[account];
+     * @dev See {BEP20-_beforeTokenTransfer}. See {BEP20Capped-_beforeTokenTransfer}.
+     */
+    function _beforeTokenTransfer(address from, address to, uint256 amount) internal override {
+        super._beforeTokenTransfer(from, to, amount);
+        
+        if (from == address(0)) { // When minting tokens
+          require(can_invest, "Issue of new shares has been restricted by the fund initiator / owner.");
+        }
     }
     
-    /**
-    * @dev See {BEP20-transfer}.
-    *
-    * Requirements:
-    *
-    * - `recipient` cannot be the zero address.
-    * - the caller must have a balance of at least `amount`.
-    */
-    function transfer(address recipient, uint256 amount) external override returns (bool) {
-        _transfer(_msgSender(), recipient, amount);
-        return true;
-    }
-    
-    /**
-    * @dev See {BEP20-allowance}.
-    */
-    function allowance(address owner, address spender) external view override returns (uint256) {
-        return _allowances[owner][spender];
-    }
-    
-    /**
-    * @dev See {BEP20-approve}.
-    *
-    * Requirements:
-    *
-    * - `spender` cannot be the zero address.
-    */
-    function approve(address spender, uint256 amount) external override returns (bool) {
-        _approve(_msgSender(), spender, amount);
-        return true;
-    }
-    
-    /**
-    * @dev See {BEP20-transferFrom}.
-    *
-    * Emits an {Approval} event indicating the updated allowance. This is not
-    * required by the EIP. See the note at the beginning of {BEP20};
-    *
-    * Requirements:
-    * - `sender` and `recipient` cannot be the zero address.
-    * - `sender` must have a balance of at least `amount`.
-    * - the caller must have allowance for `sender`'s tokens of at least
-    * `amount`.
-    */
-    function transferFrom(address sender, address recipient, uint256 amount) external override returns (bool) {
-        _transfer(sender, recipient, amount);
-        _approve(sender, _msgSender(), _allowances[sender][_msgSender()].sub(amount, "BEP20: transfer amount exceeds allowance"));
-        return true;
-    }
-    
-    /**
-    * @dev Atomically increases the allowance granted to `spender` by the caller.
-    *
-    * This is an alternative to {approve} that can be used as a mitigation for
-    * problems described in {BEP20-approve}.
-    *
-    * Emits an {Approval} event indicating the updated allowance.
-    *
-    * Requirements:
-    *
-    * - `spender` cannot be the zero address.
-    */
-    function increaseAllowance(address spender, uint256 addedValue) public returns (bool) {
-        _approve(_msgSender(), spender, _allowances[_msgSender()][spender].add(addedValue));
-        return true;
-    }
-    
-    /**
-    * @dev Atomically decreases the allowance granted to `spender` by the caller.
-    *
-    * This is an alternative to {approve} that can be used as a mitigation for
-    * problems described in {BEP20-approve}.
-    *
-    * Emits an {Approval} event indicating the updated allowance.
-    *
-    * Requirements:
-    *
-    * - `spender` cannot be the zero address.
-    * - `spender` must have allowance for the caller of at least
-    * `subtractedValue`.
-    */
-    function decreaseAllowance(address spender, uint256 subtractedValue) public returns (bool) {
-        _approve(_msgSender(), spender, _allowances[_msgSender()][spender].sub(subtractedValue, "BEP20: decreased allowance below zero"));
-        return true;
-    }
-    
-    /**
-    * @dev Creates `amount` tokens and assigns them to `msg.sender`, increasing
-    * the total supply.
-    *
-    * Requirements
-    *
-    * - `msg.sender` must be the token owner
-    */
-    function mint(uint256 amount) public onlyOwner returns (bool) {
-        _mint(_msgSender(), amount);
-        return true;
-    }
-    
-    /**
-    * @dev Moves tokens `amount` from `sender` to `recipient`.
-    *
-    * This is internal function is equivalent to {transfer}, and can be used to
-    * e.g. implement automatic token fees, slashing mechanisms, etc.
-    *
-    * Emits a {Transfer} event.
-    *
-    * Requirements:
-    *
-    * - `sender` cannot be the zero address.
-    * - `recipient` cannot be the zero address.
-    * - `sender` must have a balance of at least `amount`.
-    */
-    function _transfer(address sender, address recipient, uint256 amount) internal {
-        require(sender != address(0), "BEP20: transfer from the zero address");
-        require(recipient != address(0), "BEP20: transfer to the zero address");
-    
-        _balances[sender] = _balances[sender].sub(amount, "BEP20: transfer amount exceeds balance");
-        _balances[recipient] = _balances[recipient].add(amount);
-        emit Transfer(sender, recipient, amount);
-    }
-    
-    /** @dev Creates `amount` tokens and assigns them to `account`, increasing
-    * the total supply.
-    *
-    * Emits a {Transfer} event with `from` set to the zero address.
-    *
-    * Requirements
-    *
-    * - `to` cannot be the zero address.
-    */
-    function _mint(address account, uint256 amount) internal {
-        require(account != address(0), "BEP20: mint to the zero address");
-    
-        _totalSupply = _totalSupply.add(amount);
-        _balances[account] = _balances[account].add(amount);
-        emit Transfer(address(0), account, amount);
-    }
-    
-    /**
-    * @dev Destroys `amount` tokens from `account`, reducing the
-    * total supply.
-    *
-    * Emits a {Transfer} event with `to` set to the zero address.
-    *
-    * Requirements
-    *
-    * - `account` cannot be the zero address.
-    * - `account` must have at least `amount` tokens.
-    */
-    function _burn(address account, uint256 amount) internal {
-        require(account != address(0), "BEP20: burn from the zero address");
-    
-        _balances[account] = _balances[account].sub(amount, "BEP20: burn amount exceeds balance");
-        _totalSupply = _totalSupply.sub(amount);
-        emit Transfer(account, address(0), amount);
-    }
-    
-    /**
-    * @dev Sets `amount` as the allowance of `spender` over the `owner`s tokens.
-    *
-    * This is internal function is equivalent to `approve`, and can be used to
-    * e.g. set automatic allowances for certain subsystems, etc.
-    *
-    * Emits an {Approval} event.
-    *
-    * Requirements:
-    *
-    * - `owner` cannot be the zero address.
-    * - `spender` cannot be the zero address.
-    */
-    function _approve(address owner, address spender, uint256 amount) internal {
-        require(owner != address(0), "BEP20: approve from the zero address");
-        require(spender != address(0), "BEP20: approve to the zero address");
-    
-        _allowances[owner][spender] = amount;
-        emit Approval(owner, spender, amount);
-    }
-    
-    /**
-    * @dev Destroys `amount` tokens from `account`.`amount` is then deducted
-    * from the caller's allowance.
-    *
-    * See {_burn} and {_approve}.
-    */
-    function _burnFrom(address account, uint256 amount) internal {
-        _burn(account, amount);
-        _approve(account, _msgSender(), _allowances[account][_msgSender()].sub(amount, "BEP20: burn amount exceeds allowance"));
-    }
-
     /**
      * @dev Return initiator address 
      * @return address of initiator
@@ -324,5 +266,63 @@ contract Fund is Context, IBEP20, Ownable {
      */
     function getFundStatus() external view returns (FundStatus) {
         return fund_status;
+    }
+    
+    /**
+     * @dev Return fund's asset base currency
+     * @return the fund's asset base currency
+     */
+    function getBaseCurrency() external view returns (address) {
+        return asset_base_currency;
+    }
+    
+    /**
+     * @dev Function to issue shares once funding is received.
+     *
+     * NOTE: Restricting function to owner only.
+     *
+     * @param _sharesReceiver The address of the new fund manager
+     */
+    function issueShares(address _sharesReceiver, uint256 _amount) public onlyOwner {
+        _mint(_sharesReceiver, _amount); // Mint the new tokens and issue to shares receiver; No normalization done to _amount, frontend to handle
+    }
+    
+    /**
+    * @dev Stop new investments
+    *
+    * Requirements
+    *
+    * - `msg.sender` must be the fund initiator or owner
+    */
+    function stopNewInvestments() public returns (bool) {
+        require(_msgSender() == fund_initiator || _msgSender() == owner(), "Caller is not fund initiator or owner.");
+        
+        can_invest = false;
+        return true;
+    }
+    
+    /**
+    * @dev Resume new investments
+    *
+    * Requirements
+    *
+    * - `msg.sender` must be the fund initiator or owner
+    */
+    function resumeNewInvestments() public returns (bool) {
+        require(_msgSender() == fund_initiator || _msgSender() == owner(), "Caller is not fund initiator or owner.");
+        
+        can_invest = true;
+        return true;
+    }
+    
+    /**
+     * @dev Function to change fund manager.
+     *
+     * NOTE: Restricting function to owner only.
+     *
+     * @param _newFundMgr The address of the new fund manager
+     */
+    function changeFundManager(address payable _newFundMgr) public onlyOwner {
+        fund_manager = _newFundMgr;
     }
 }
---
+++ contracts/FundPlatform.sol
@@ -0,0 +1,112 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.8.0;
+
+import * as bridgeUSDT from "./IBEP40.sol";
+import "./Fund.sol";
+
+contract FundPlatform is BEP20Mintable, BEP20Burnable {
+    using SafeMath for uint256;
+    
+    mapping(bytes32 => Fund) funds; // Tracks all the funds created on the platform; Maps fund symbol to Fund instance
+    mapping(address => FundManager) fundMgrs; // Tracks all the fund managers of each fund; Maps Fund contract address to FundManager instance
+    address[] public fundListing;
+    address payable platformFeeReceiver;
+    address public asset_base_currency;
+    
+    /**
+     * @dev Setup the mutual fund platform
+     */
+    constructor(
+        uint256 _initialAmount,
+        string memory _tokenName,
+        string memory _tokenSymbol,
+        address _assetBaseCurrency,
+        address payable _feeReceiver
+    )
+        BEP20(_tokenName, _tokenSymbol)
+        payable 
+    { 
+        //_setupDecimals(_decimals);                            // Amount of decimals for display purposes; Set to the default 18
+        
+        _mint(_msgSender(), _initialAmount);                    // Mint the new platform tokens and issue to platform contract deployer; No normalization done
+        
+        asset_base_currency = _assetBaseCurrency;                       // Can only be set once during setup; Immutable
+        platformFeeReceiver = _feeReceiver;                             // Can only be set once during setup; Immutable
+    }
+    
+    /**
+     * @dev Initiate the creation of a decentralized mutual fund and track the fund listings
+     */
+    function createFund(
+        uint256 _initialAmount, 
+        string memory _tokenName, 
+        string memory _tokenSymbol, 
+        Fund.FundType _fundType, 
+        address _assetBaseCurrency, 
+        uint256 _seedFunding, 
+        address payable _fundManager
+    ) public payable returns (Fund) {
+        require(funds[_toBytes32(_tokenSymbol)] == Fund(0), "Fund symbol is in use, kindly use a unique fund symbol."); // Check if the fund symbol is already in use
+        require(_initialAmount > 0, "Initial number of share supply cannot be zero.");
+        
+        // Caller of this function is likely the fund initiator; This fund platform contract will be the "owner" of the fund contract
+        Fund _fundInstance = new Fund{value: msg.value}(_tokenName, _tokenSymbol, _fundType, _assetBaseCurrency, platformFeeReceiver, _fundManager, _msgSender());
+        
+        address payable _fundAddress = payable(address(_fundInstance));
+        
+        fundListing.push(_fundAddress);
+       
+        bridgeUSDT.IBEP40(_assetBaseCurrency).transferFrom(_msgSender(), _fundAddress, _seedFunding);
+        
+        _fundInstance.issueShares(_msgSender(), _initialAmount);
+        
+        funds[_toBytes32(_tokenSymbol)] = _fundInstance; // Use helper function to convert the string to bytes32 mapping key
+        fundMgrs[_fundAddress] = FundManager(_fundManager); // Explicitly type convert the _feeReceiver address into FundManager instance
+        
+        return _fundInstance;
+    }
+    
+    /**
+     * @dev Retrieve a single fund using its symbol
+     */
+    function getAllFunds() public view returns (address[] memory) {
+        return fundListing;
+    }
+    
+    /**
+     * @dev Retrieve a single fund using its symbol
+     */
+    function getFund(string memory _fundSymbol) public view returns (address payable) {
+        return payable(address(funds[_toBytes32(_fundSymbol)]));
+    }
+    
+    /**
+     * @dev Helper function to convert string to bytes32
+     */
+    function _toBytes32(string memory _origString) private pure returns (bytes32) {
+        return keccak256(abi.encode(_origString));
+    }
+    
+    /**
+     * @dev Enable platform fee withdrawals
+     */
+    function withdraw(uint256 amount) public onlyOwner {
+        payable(owner()).transfer(amount);
+    }
+    
+    /**
+     * @dev Invest in a fund using its symbol
+     */
+    function investFund(string memory _fundSymbol, address _assetCurrency, uint256 _investAmount) public payable returns (bool) {
+        require(_assetCurrency == 0x337610d27c682E347C9cD60BD4b3b107C9d34dDd || _assetCurrency == 0x55d398326f99059fF775485246999027B3197955, "Currency is not set as Binance wrapped USDT.");
+        
+        address payable _investingFund = getFund(_fundSymbol);
+        
+        require(_investingFund != address(0), "Investing fund cannot be zero address.");
+        
+        bridgeUSDT.IBEP40(_assetCurrency).transferFrom(_msgSender(), _investingFund, _investAmount);
+        
+        return true;
+    }
+}
